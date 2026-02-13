e717caad80b610201f379d350ab8c35ab5558e6d
Sumit Khandelwal <sumitkhandelwal547@gmail.com>
2026-02-14 00:44:16 +0530

    Remove dotenv configuration from App.jsx
    Removed dotenv configuration from App component.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,9 +6,6 @@ import Add from './pages/Add/Add.jsx'
 import List from './pages/List/List.jsx'
 import Order from './pages/Orders/Order.jsx'
 import { ToastContainer } from 'react-toastify';
-import dotenv from "dotenv";
-dotenv.config();
-
 
 const App = () => {
 
